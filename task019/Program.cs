ca35e40ce75a0c38eecd153035d389867e41255d
Alexander Kamaev <qalex2@yandex.ru>
2023-07-14 11:26:57 +0300

    Все задачи решены!

---
+++ task019/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool CheckPal(int numberw)
+{
+    int originalN = numberw;
+    int reverseN = 0;
+
+    while (numberw > 0)
+    {
+        int digit = numberw % 10;
+        reverseN = reverseN * 10 + digit;
+        numberw /= 10;
+    }
+    return originalN == reverseN;
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = GetInput("Введите проверяемое число N: ");
+int numberw = Math.Abs(number);
+
+bool isPal = CheckPal(numberw);
+
+if (isPal) Console.WriteLine($"Введенное число {number} является палиндромом!");
+else Console.WriteLine($"Введенное число {number} НЕ является палиндромом!");
--- task023/Program.cs
+++ task023/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 23
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
+// 3 -> 1, 8, 27  
 // 5 -> 1, 8, 27, 64, 125
 
 int GetInput(string text)
@@ -9,17 +9,27 @@ int GetInput(string text)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-void NumberOutput (int n)
+void NumberOutput(int n)
 {
-Console.WriteLine($"Кубы чисел в промежутке от 1 до {n}:", n);
-
-for (int i = 1; i < n; i++)
-{
-    Console.Write(Math.Pow(i, 3)+", ");
+    Console.WriteLine($"Кубы чисел в промежутке от 0 до {n}:", n);
+    if (n == 0) Console.Write(n);
+    if (n > 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Math.Pow(i, 3) + ", ");
+        }
+        Console.Write(Math.Pow(n, 3));
+    }
+    if (n < 0)
+    {
+        for (int i = n; i < -1; i++)
+        {
+            Console.Write(Math.Pow(i, 3) + ", ");
+        }
+        Console.Write("-1, 0");
+    }
 }
-Console.Write(Math.Pow(n, 3));
-}
+    int n = GetInput("Введите число N: ");
 
-int n = Math.Abs(GetInput("Введите число N: "));
-
-NumberOutput(n);
+    NumberOutput(n);
